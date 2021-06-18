f88effd582d0eecd4b7acfe854a8247d7a6c15e9
YSThomas <tombrazy@gmail.com>
2021-06-18 19:16:10 +0700

    promise in reateTournament

--- src/store/modules/tournament.js
+++ src/store/modules/tournament.js
@@ -7,8 +7,8 @@ export default ({
   state: {
     tournament: null, // Сам турнир
     participantsCount: null, // Кол-во участников
-    matchList: null,
-    roundList: null,
+    matchList: [],
+    roundList: [],
   },
   mutations: {
     SET_PARTICIPANTS_COUNT(state, data){ // Установка количества участников
@@ -29,18 +29,21 @@ export default ({
   },
   actions: {
     createTournament({commit, getters, dispatch}, data){ // Создание турнира
-      data = Number(data)
-
-      if(typeof data === 'number' && data === Math.pow(2,Math.floor(Math.log(data)/Math.log(2)))) {
-        commit('SET_PARTICIPANTS_COUNT', data)
-        commit('SET_TOURNAMENT')
-        dispatch('defineRoundsCount')
-        dispatch('createRounds')
-        dispatch('createMatches')
-        console.log(getters.getTournament)
-      }else{
-        throw Error('Количество игроков должно быть: 2, 4, 8, 16, 32, 64...')
-      }
+      return new Promise((resolve, reject)=>{
+        data = Number(data)
+        if(typeof data === 'number' && data > 1 && data === Math.pow(2,Math.floor(Math.log(data)/Math.log(2)))) {
+          commit('SET_PARTICIPANTS_COUNT', data)
+          commit('SET_TOURNAMENT')
+          dispatch('defineRoundsCount')
+          dispatch('createRounds')
+          dispatch('createMatches')
+          resolve()
+        }else{
+          reject('Количество игроков должно быть: 2, 4, 8, 16, 32, 64...')
+        }
+      }).catch(error =>{
+        console.log(error)
+      })
     },
     defineRoundsCount({ state, commit }){ // Определяет и устанавливает кол-во раундов
       let num = 0
