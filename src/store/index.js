790f0743d36ca10bfdd46038014f0077f5f3dd86
YSThomas <tombrazy@gmail.com>
2021-06-22 19:32:11 +0700

    Store refactoring

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import {createStore} from 'vuex'
-import tournament from "./modules/tournament";
+import tournament from "./modules/moduleTournament/tournament";
 
 export default createStore({
   state: {},
